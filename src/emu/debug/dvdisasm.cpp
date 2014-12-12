e9b30cbedcc86a34fc6bbe0b07d93c111634cd3b
Fabrice Bellet <fabrice@bellet.info>
2014-12-12 13:43:01 +0100

    debug: tweak spacing in the asm debugger view

--- src/emu/debug/dvdisasm.cpp
+++ src/emu/debug/dvdisasm.cpp
@@ -47,7 +47,7 @@ debug_view_disasm_source::debug_view_disasm_source(std::string &&name, device_t 
 debug_view_disasm::debug_view_disasm(running_machine &machine, debug_view_osd_update_func osdupdate, void *osdprivate)
 	: debug_view(machine, DVT_DISASSEMBLY, osdupdate, osdprivate),
 		m_right_column(DASM_RIGHTCOL_RAW),
-		m_backwards_steps(3),
+		m_backwards_steps(6),
 		m_dasm_width(DEFAULT_DASM_WIDTH),
 		m_previous_pc(1),
 		m_expression(machine)
